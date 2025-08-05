9e8140843fc8e3dca853b3358755a32f4bbe5669
queren-somarriba <qsomarri@f6r8s14.clusters.42paris.fr>
2025-08-05 19:12:13 +0200

    add make_listening_socket function

--- server.cpp
+++ server.cpp
@@ -7,36 +7,44 @@
 #include <unistd.h>
 #include <cstring>
 
-#define PORT "8081"
+#define PORT "8080"
 #define IP "127.0.0.1"
 
-int main()
+int make_listening_socket()
 {
-	//1 - creation socket;
+	/*1 - creation socket*/
 	struct addrinfo hint;
 	struct addrinfo *addr;
 	//struct addrinfo *res;
 	std::memset(&hint, 0, sizeof(hint));
-	hint.ai_family = AF_INET;
 	hint.ai_flags = AI_PASSIVE;
+	hint.ai_family = AF_INET;
 	hint.ai_socktype = SOCK_STREAM;
 	if (getaddrinfo(IP, PORT, &hint, &addr) != 0)
-		return (std::cerr << "addrinfo" << std::endl, 1);
+		return (std::cerr << "Error: addrinfo" << std::endl, -1);
 	int socket_fd = socket(addr->ai_family, addr->ai_socktype, 0);
 	if (socket_fd < 0)
-		return (std::cerr << "socket creation" << std::endl, 1);
-	//2 - bind socket
-	//std::cout << "socket_fd= " << socket_fd << ", ai_addr= " << addr->ai_addr->sa_data << ", ai_addrlen= " << addr->ai_addrlen << std::endl;
+		return (std::cerr << "Error: socket creation" << std::endl, -1);
+	/*2 - bind socket*/
 	if (bind(socket_fd, addr->ai_addr, addr->ai_addrlen) != 0)
-		return (std::cerr << "bind socket" << std::endl, 1);
-	//3 - listen && accept
-	if (listen(socket_fd, 1001) < 0)
-		return (std::cerr << "listen" << std::endl, 1);
-	int new_socket = accept(socket_fd, NULL, 0);
-	if (new_socket < 0)
-		return (std::cerr << "accept" << std::endl, 1);
+		return (std::cerr << "Error: bind socket" << std::endl, -1);
 	freeaddrinfo(addr);
-	close(new_socket);
+	/*3 - listen && accept*/
+	if (listen(socket_fd, 1001) < 0)
+		return (std::cerr << "Error: listen" << std::endl, -1);
+	return (socket_fd);
+}
+
+int main()
+{
+	int socket_fd = make_listening_socket();
+	if (socket_fd < 0)
+		return (1);
+	int client_socket = accept(socket_fd, NULL, 0);
+	if (client_socket < 0)
+		return (std::cerr << "Error: accept" << std::endl, 1);
+	std::cout << "Connection accept for new client" << std::endl; 
+	close(client_socket);
 	close(socket_fd);
 	return (0);
 }
