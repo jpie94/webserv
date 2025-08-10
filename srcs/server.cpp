d6b5cf482a0bc0981f962d337566236f77b1fae1
queren-somarriba <qsomarri@f4r2s6.clusters.42paris.fr>
2025-08-10 18:38:42 +0200

    replace array pollfd by vector, add signal

--- srcs/server.cpp
+++ srcs/server.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdio>
 #include <string>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -9,9 +10,17 @@
 #include <poll.h>
 #include <vector>
 #include <exception>
+#include <sstream>
+#include <csignal>
 
 #define PORT "8081"
 #define IP "127.0.0.1"
+
+void throw_error(const char* msg)
+{
+    std::perror(msg);
+    throw std::runtime_error(msg);
+}
 
 int	make_listening_socket()
 {
@@ -23,122 +32,123 @@ int	make_listening_socket()
 	hint.ai_family = AF_INET;
 	hint.ai_socktype = SOCK_STREAM;
 	if (getaddrinfo(IP, PORT, &hint, &addr) != 0)
-	{
-		std::cerr << "Error: addrinfo" << std::endl;
-		throw std::exception();
-	}
+		throw_error("getaddrinfo");
 	int socket_fd = socket(addr->ai_family, addr->ai_socktype, 0);
 	if (socket_fd < 0)
 	{
 		freeaddrinfo(addr);
-		std::cerr << "Error: socket creation" << std::endl;
-		throw std::exception();
+		throw_error("socket");
 	}
 	if (bind(socket_fd, addr->ai_addr, addr->ai_addrlen) != 0)
 	{
 		freeaddrinfo(addr);
-		std::cerr << "Error: bind socket" << std::endl;
-		throw std::exception();
+		throw_error("bind");
 	}
 	freeaddrinfo(addr);
 	if (listen(socket_fd, 1001) < 0)
-	{
-		std::cerr << "Error: listen" << std::endl;
-		throw std::exception();
-	}
+		throw_error("listen");
 	return (socket_fd);
 }
 
-struct pollfd*	add_client_to_pollfds(struct pollfd *pfds, int socket_fd, int server_fd, nfds_t &len)
+void	add_client_to_pollfds(std::vector<struct pollfd> &pfds, int server_fd)
 {
+	int socket_fd;
 	socket_fd = accept(server_fd, NULL, 0); // On est sur un socket server en position POLLIN
 	if (socket_fd < 0)
-	{
-		std::cerr << "Error: accept" << std::endl;
-		throw std::exception();
-	}
-	struct pollfd *new_pollfd = new struct pollfd [len + 1];
-	if (!new_pollfd)
-	{
-		std::cerr << "Error: new" << std::endl;
-		throw std::exception();
-	}
-	for (nfds_t j = 0; j < len; ++j)
-		new_pollfd[j] = pfds[j];
-	delete [] pfds;
-	std::memset(new_pollfd, 0, sizeof(struct pollfd *) * (len + 1));
-	new_pollfd[len - 1].fd = socket_fd;
-	new_pollfd[len - 1].events = POLLIN | POLLOUT;
-	(len)++;
+		throw_error("accept");
+	struct pollfd temp = {socket_fd, POLLIN, 0};
+	pfds.push_back(temp);
 	std::cout << "Connection accepted for new client "<< socket_fd << std::endl;
-	return (new_pollfd);
 }
 
-struct pollfd*	send_msg_to_client(struct pollfd *pfds, nfds_t &len, nfds_t j)
+void	erase_from_pollfd(std::vector<struct pollfd> &pfds, nfds_t &j)
+{
+	if (close(pfds[j].fd) < 0)
+		throw_error("close");
+	pfds.erase(pfds.begin() + j--);
+}
+
+void	send_msg_to_client(int client_fd, const char *message)
 {
 	std::cout << "Socket client en position POLLIN : envoi d un message" << std::endl;
-	const char *message = "lol\n";
+	ssize_t msg_len = std::strlen(message);
 	ssize_t count = 0;
-	while (count < static_cast<ssize_t>(std::strlen(message)))
-		count += send(pfds[j].fd, message, std::strlen(message), 0);
-	if (close(pfds[j].fd) < 0)
+	ssize_t	sent = 0;
+	while (count < msg_len)
 	{
-		std::cerr << "Error: close" << std::endl;
-		throw std::exception();
+		sent = send(client_fd, message + count, msg_len - count, 0);
+		if (sent < 0)
+			throw_error("send");
+		count += sent;
 	}
-	struct pollfd *new_pollfd = new struct pollfd [len - 1];
-	if (!new_pollfd)
+	std::cout << "\ntaille message envoye: " << count << '\n';
+	//send(client_fd,"\nOK\n", 4, 0);
+}
+
+void	handle_client_request(std::vector<struct pollfd> &pfds, nfds_t &j, std::vector<char> &buffer)
+{
+	int bytes_read, sender_fd;
+	sender_fd = pfds[j].fd;
+	char buf[BUFSIZ];
+	std::memset(&buf, '\0', sizeof(buf));
+	int i = -1;
+	bytes_read = recv(sender_fd, &buf, BUFSIZ, 0);
+	if (bytes_read < 0)
+		throw_error("recv");
+	if (bytes_read == 0)
 	{
-		std::cerr << "Error: new" << std::endl;
-		throw std::exception();
+		std::cout << "[" << sender_fd << "] Client socket closed connection." << '\n';
+		erase_from_pollfd(pfds, j);
 	}
-	int k = -1;
-	for (nfds_t i = 0; i < len; ++i)
-		if (i != j)
-			new_pollfd[++k] = pfds[i];
-	delete [] pfds;
-	len--;
-	return (new_pollfd);
+	i = -1;
+	while (buf[++i])
+		buffer.push_back(buf[i]);
+	std::cout << "\ntaille message recu: " << buffer.size() << '\n';
+	std::cout << "[" << sender_fd << "] Got message: " << buffer.data() << '\n';
+	//transform cleint request into answer
+	send_msg_to_client(sender_fd, buffer.data());
+}
+
+void sigIntHandler(int signal)
+{
+	if (signal == SIGINT)
+		std::exception();	
 }
 
 int	main()
 {
 	try
 	{
-		nfds_t len = 1;
-		struct pollfd *pfds = new struct pollfd[len];
-		int socket_fd = 0;
+		std::signal(SIGINT, sigIntHandler);
+		std::vector<struct pollfd> pfds;
+		std::vector<char> buffer;
 		int status, server_fd = make_listening_socket();
 		if (server_fd < 0)
 			return (1);
-		std::memset(pfds, 0, sizeof(struct pollfd *) * len);
-		pfds[len - 1].fd = server_fd;
-		pfds[len - 1].events = POLLIN;
+		struct pollfd temp = {server_fd, POLLIN, 0};
+		pfds.push_back(temp);
 		while(1)
 		{
-			status = poll(pfds, len, 2000);
+			std::memset(&buffer, '\0', sizeof(buffer));
+			status = poll(pfds.data(), pfds.size(), 2000);
 			if (status < 0)
-				return (std::cerr << "Error: poll" << std::endl, 1);
+				throw_error("poll");
 			if (status == 0)
 			{
-				std::cout << "Waiting for connection" << std::endl; 
+				std::cout << "Waiting for connection..." << std::endl; 
 				continue;
 			}
-			for (nfds_t j = 0; j < len; ++j)
+			for (nfds_t j = 0; j < pfds.size(); ++j)
 			{
-				if ((pfds[j].revents & POLLIN) != 1)
+				if (!(pfds[j].revents & POLLIN) && !(pfds[j].revents & POLLOUT))
 					continue;
 				std::cout << "Ready for I/O operation" << '\n';
-				if (pfds[j].fd == server_fd)
-					pfds = add_client_to_pollfds(pfds, socket_fd, server_fd, len);
-				else if (pfds[j].fd != server_fd) // On est sur un socket client en position POLLIN
-					pfds = send_msg_to_client(pfds, len, j);
-				// else if ((pfds[j].fd != server_fd) && (pfds[j].revents & POLLOUT) == 1) // On est sur un socket client en position POLLOUT
-				// 	std::cout << "Socket client en position POLLIN" << std::endl;
-				// else if ((pfds[j].revents & POLLOUT) == 1 && (pfds[j].fd == server_fd))
-				// 	std::cout << "LE SERVEUR EN POLLOUT" << std::endl;
-				// else
-				// 	std::cout << "autre wtf" << '\n';
+				if (pfds[j].fd == server_fd && (pfds[j].revents & POLLIN))
+					add_client_to_pollfds(pfds, server_fd);
+				else if (pfds[j].fd != server_fd && (pfds[j].revents & POLLIN)) // On est sur un socket client en position POLLIN
+					handle_client_request(pfds, j, buffer);
+				// if (pfds[j].fd != server_fd && (pfds[j].revents & POLLOUT))
+				// 	send_msg_to_client(pfds[j].fd , buffer);
 			}
 		}
 	}
